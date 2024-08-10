0cb76f827a2b66282d0a62773faa82261bd98e05
“JonathasVeras” <jonathasveras42@gmail.com>
2024-08-10 09:02:46 -0300

    feature: interface for characterSheet

---
+++ src/interfaces/characterSheet.ts
@@ -0,0 +1,47 @@
+interface IAttributes {
+  acceptance: number;
+  agility: number;
+  selfControl: number;
+  blocking: number;
+  fighting: number;
+  body: number;
+  destiny: number;
+  dexterity: number;
+  empathy: number;
+  dodge: number;
+  faith: number;
+  focus: number;
+  strength: number;
+  humanity: number;
+  intelligence: number;
+  intimidation: number;
+  hacking: number;
+  leadership: number;
+  narrative: number;
+  protection: number;
+  endurance: number;
+  wisdom: number;
+  seduction: number;
+  luck: number;
+}
+
+export interface ICharacterSheet {
+  background: string;
+  attributes: IAttributes;
+  difficultTerrainMovement: number;
+  run: number;
+  woundedAndOverloaded: number;
+  userId: string;
+  age: string;
+  identity: number;
+  movement: number;
+  name: string;
+  passivePerception: number;
+  maxWeight: number;
+  tokenHealthPoints: number;
+  damageReduction: number;
+  maxSanity: number;
+  gender: string;
+  baseSpeed: number;
+  maxHealth: number;
+}
